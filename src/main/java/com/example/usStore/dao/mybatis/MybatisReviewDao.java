4dcc454c278b84dc80b756ffbb845181b334d78c
leejieuns2 <2jjingni@gmail.com>
2020-09-29 21:50:19 +0900

    [hotfix] remove remark

--- src/main/java/com/example/usStore/dao/mybatis/MybatisReviewDao.java
+++ src/main/java/com/example/usStore/dao/mybatis/MybatisReviewDao.java
@@ -1,32 +1,35 @@
 package com.example.usStore.dao.mybatis;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-//import com.example.usStore.dao.ReviewDao;
-//import com.example.usStore.dao.mybatis.mapper.ReviewMapper;
-//import com.example.usStore.domain.Review;
+import com.example.usStore.dao.ReviewDao;
+import com.example.usStore.dao.mybatis.mapper.ReviewMapper;
+import com.example.usStore.domain.Review;
 
 @Transactional
 @Qualifier("mybatisReviewDao")
 @Repository
-public class MybatisReviewDao {
-//   @Autowired
-//   private ReviewMapper reviewMapper;
-//   
-//   @Override
-//   public void insertReview(Review review) {
-//      reviewMapper.insertReview(review);
-//   }
-//
-//   @Override
-//   public void deleteReview(int itemId){
-//      reviewMapper.deleteReview(itemId);
-//   }
-//
-//   @Override
-//   public List<Review> getReviewListByItemId(int itemId) {
-//      return reviewMapper.getReviewListByItemId(itemId);
-//   }
+public class MybatisReviewDao implements ReviewDao{
+	
+   @Autowired
+   private ReviewMapper reviewMapper;
+   
+   @Override
+   public void insertReview(Review review) {
+      reviewMapper.insertReview(review);
+   }
+
+   @Override
+   public void deleteReview(int itemId){
+      reviewMapper.deleteReview(itemId);
+   }
+
+   @Override
+   public List<Review> getReviewListByItemId(int itemId) {
+      return reviewMapper.getReviewListByItemId(itemId);
+   }
 }
--- src/main/java/com/example/usStore/dao/mybatis/mapper/ReviewMapper.java
+++ src/main/java/com/example/usStore/dao/mybatis/mapper/ReviewMapper.java
@@ -1,14 +1,14 @@
 package com.example.usStore.dao.mybatis.mapper;
 
-//
-//import com.example.usStore.domain.Review;
-//
+import java.util.List;
+import com.example.usStore.domain.Review;
+
 public interface ReviewMapper {
-//	
-//   void insertReview(Review review);
-//
-//   void deleteReview(int itemId);
-//
-//   List<Review> getReviewListByItemId(int itemId);
-//   
+	
+   void insertReview(Review review);
+
+   void deleteReview(int itemId);
+
+   List<Review> getReviewListByItemId(int itemId);
+   
 }
