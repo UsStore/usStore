dac25b312e351ba122a75effcb7fbd1c223b2b50
angelatto <pudri109@gmail.com>
2020-10-05 21:35:21 +0900

    fix : qualifier수정

--- src/main/java/com/example/usStore/dao/mybatis/MybatisUniversityDao.java
+++ src/main/java/com/example/usStore/dao/mybatis/MybatisUniversityDao.java
@@ -2,11 +2,14 @@ package com.example.usStore.dao.mybatis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 
 import com.example.usStore.dao.UniversityDao;
 import com.example.usStore.dao.mybatis.mapper.UniversityMapper;
 import com.example.usStore.domain.University;
 
+
+@Repository
 public class MybatisUniversityDao implements UniversityDao{
 	
 	@Autowired
